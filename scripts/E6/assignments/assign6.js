34067e7d594632cb0c11b3a01b9ae8d5c274929b
sereena <sereenaeldhose@gmail.com>
2024-02-05 15:47:29 +0530

    updates

--- scripts/E6/assignments/assign6.js
+++ scripts/E6/assignments/assign6.js
@@ -14,3 +14,23 @@ printPersion = ({ name, age, address: { city: city, country: country } }) => {
 };
 
 printPersion(person);
+
+
+// Another way
+
+ displayPerson = () =>{
+   // const { name: fullName, age:age1, address: { city: current_city, country: nation = "India" } } = person  or 
+   const { name: fullName, age:age1, address: { city: current_city, country: nation = "USA" } } =  {
+    name: "David",
+    age: 30,
+    address: {
+      city: "Delhi",
+      country: "India",
+    }
+}
+    console.log(
+        `Person Info : \n Name = ${fullName} \n Age = ${age1} \n Address = \n City : ${current_city} \n Country : ${nation}`
+      );
+
+ }
+ displayPerson();

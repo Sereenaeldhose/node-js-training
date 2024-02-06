481f967d23dd0d007ef31909539cb3f6c7e5e0f7
sereena <sereenaeldhose@gmail.com>
2024-02-06 18:39:00 +0530

    updates

--- scripts/E6/assignments/assign8.js
+++ scripts/E6/assignments/assign8.js
@@ -1,67 +1,42 @@
-// async callback fn
-var array = [];
-addPost = async () => {
-  return new  Promise((resolve) => {
-    pushValues();
-    console.log("Pushing to array completed..");
-    resolve(array);
-  });
+let posts = [];
+pushValues = () => {
+  posts.push({ id: 1, title: "Tittle 1" });
+  posts.push({ id: 2, title: "Tittle 2" });
+  posts.push({ id: 3, title: "Tittle 3" });
+  posts.push({ id: 4, title: "Tittle 4" });
+  posts.push({ id: 5, title: "Tittle 5" });
+  posts.push({ id: 6, title: "Tittle 6" });
+  posts.push({ id: 7, title: "Tittle 7" });
+  posts.push({ id: 8, title: "Tittle 8" });
 };
 
-pushValues = () => {
-
-  array.push({ id: 1, title: "Tittle 1" });
-  array.push({ id: 2, title: "Tittle 2" });
-  array.push({ id: 3, title: "Tittle 3" });
-  array.push({ id: 4, title: "Tittle 4" });
-  array.push({ id: 5, title: "Tittle 5" });
-  array.push({ id: 6, title: "Tittle 6" });
-  array.push({ id: 7, title: "Tittle 7" });
-  array.push({ id: 8, title: "Tittle 8" });
+addPost = () => {
+  return new Promise((resolve, reject) => {
+    pushValues();
+    if (posts.length === 0) {
+      reject("No values present!");
+    }
+    console.log("Pushing to array completed...");
+    resolve(posts);
+  });
 };
 
 getPosts = () => {
-  console.log("Printing the array posts.....");
-  array.forEach((element) => {
-    console.log(`id = ${element.id} and title = ${element.title}`);
-  });
-};
-
-
-   addPost().then(getPosts);
-
-
-   setTimeout(() =>{console.log("-------------- Invoking next function -------------")},300);
-
-// using asyn await function
-
-let posts = [
-  { id: 1, title: "A" },
-  { id: 2, title: "B" },
-  { id: 3, title: "C" },
-];
-
-addPost1 = (post) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(function () {
-      posts.push(post);
-      resolve(posts);
-    }, 300);
-  });
-};
-
-getPosts1 = () => {
   result = "";
+  console.log("Printing the array...");
   posts.forEach((element) => {
     result += `id = ${element.id} and title = ${element.title}\n`;
     return result;
   });
 };
 
-main2 = async () => {
-  let post = { id: 4, title: "D" };
-  await addPost1(post).then(getPosts1);
+handleFn = (message) => {
+  `ERROR : ${message}`;
+};
+
+main = async () => {
+  await addPost().then(getPosts, handleFn);
   console.log(result);
 };
 
-main2();
+main();

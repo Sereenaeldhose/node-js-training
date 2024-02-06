8378b6bfc30bbc5265e6931bee8c542179f62ec8
sereena <sereenaeldhose@gmail.com>
2024-02-06 17:55:15 +0530

    Assignment 8 - Asynchronise function

---
+++ scripts/E6/assignments/assign8.js
@@ -0,0 +1,54 @@
+// async callback fn
+var array =[];
+addPost = (id,title,callback)=>{
+    array.push({id:id,title:title});
+    console.log("Pushing to array completed..");
+    callback(array);
+    console.log("Process completed..");
+}
+
+getPosts = (array) =>{
+console.log("Printing the array posts.....");
+array.forEach(element => {
+    console.log(`id = ${element.id} and title = ${element.title}`);
+});
+}
+
+addPost(1,"Title 1", getPosts);
+addPost(2,"Title 2", getPosts);
+addPost(3,"Title 3", getPosts);
+
+console.log("-------------- Invoking next function -------------");
+
+// using asyn await function
+
+let posts = [
+  { id: 1, title: "A" },
+  { id: 2, title: "B" },
+  { id: 3, title: "C" },
+];
+
+addPost = (post) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(function () {
+      posts.push(post);
+      resolve(posts);
+    }, 300);
+  });
+};
+
+getPosts = (posts) => {
+  result = "";
+  posts.forEach((element) => {
+    result += `id = ${element.id} and title = ${element.title}\n`;
+    return result;
+  });
+};
+
+main = async () => {
+  let post = { id: 4, title: "D" };
+  await addPost(post).then((arrays) => getPosts(arrays));
+  console.log(result);
+};
+
+main();

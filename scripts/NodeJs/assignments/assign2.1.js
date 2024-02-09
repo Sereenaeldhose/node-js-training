3cb5be4d7fa9aed7d1a3ba0f96d5ac8f0bcb07e2
sereena <sereenaeldhose@gmail.com>
2024-02-09 17:22:06 +0530

    Assignment 2 - Local logging system

---
+++ scripts/NodeJs/assignments/assign2.1.js
@@ -0,0 +1,5 @@
+const logging = require("./assign2");
+
+logging.getInfo("Welcome to Logging System!");
+logging.getWarning("You are accessing Logging System!");
+logging.getError("You are failed to access Logging System!");
---
+++ scripts/NodeJs/assignments/assign2.js
@@ -0,0 +1,14 @@
+
+getInfo = (message) =>{
+    console.log(message);
+}
+
+getWarning = (message) =>{
+    console.warn(message);
+}
+
+getError = (message) =>{
+    console.error(message);
+}
+
+module.exports = {getInfo,getWarning,getError}

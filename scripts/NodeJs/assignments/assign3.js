a3eb8554872109aa21c20c7f383f62bb988336e1
sereena <sereenaeldhose@gmail.com>
2024-02-12 12:03:47 +0530

    Added nested folder condition to the assignment 3

--- scripts/NodeJs/assignments/assign3.js
+++ scripts/NodeJs/assignments/assign3.js
@@ -4,18 +4,22 @@ const logging = require("./assign2");
 
 
 deleteAllFiles = (dirPath)=>{
-
     if (!fs.existsSync(dirPath)) {
         console.error('Directory does not exist.');
         return;
     }
-
   fs.readdir(dirPath,(error,files)=>{
         if(error){
             logging.getError(error);
         }
         files.forEach(file => {
-            fs.unlink(path.join(__dirname,dirPath,file),(error)=>{
+           let filePath = path.join(dirPath,file);
+            if(!path.extname(filePath))
+           {
+            deleteAllFiles(filePath);
+           }
+           else{
+            fs.unlink(filePath,(error)=>{
                 if(error){
                     logging.getError(error);
                 }
@@ -23,9 +27,10 @@ deleteAllFiles = (dirPath)=>{
                     logging.getInfo(`Sucessfully removed ${file} from folder ${dirPath}`);
                 }
             })
+           }
         });
     })
 
 }
-deleteAllFiles("textFolder12");
+deleteAllFiles("textFolder");
 

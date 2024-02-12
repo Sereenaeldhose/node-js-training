606e186d1b47b49fdf63c03c188206ceaf608ae1
sereena <sereenaeldhose@gmail.com>
2024-02-12 16:10:43 +0530

    updates

--- scripts/NodeJs/assignments/assign4.js
+++ scripts/NodeJs/assignments/assign4.js
@@ -1,11 +1,22 @@
 const fs = require("node:fs");
+const logging = require("./assign2");
 
-(copyContent = () => {
-  const readableStream = fs.createReadStream("./FileToCopy/text1.txt", {
+(copyContent =  () => {
+  const readableStream =   fs.createReadStream("./FileToCopy/text1.txt", {
     encoding: "utf-8",
     highWaterMark: 2,
   });
-
-  const writeableStream = fs.createWriteStream("./FileToCopy/text2.txt");
-  readableStream.pipe(writeableStream);
+  const writeableStream =   fs.createWriteStream("./FileToCopy/text2.txt");
+    readableStream.pipe(writeableStream);
+  logging.getInfo("Successfully copied data!");
+ 
+  writeableStream.on("close", ()=>{
+    fs.createReadStream("./FileToCopy/text1.txt", {
+        encoding: "utf-8",
+        highWaterMark: 100,
+      }).on("data", (data)=>{
+        logging.getInfo(data);
+      })
+  })
 })();
+
